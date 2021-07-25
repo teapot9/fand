1bfcbf92ad8536b926d3f6076574437ea55c7406
Louis Leseur <louis.leseur@gmail.com>
2021-07-25 23:38:22 +0200

    Remove pysmart workaround
    Fixed in new pySMART release.

--- fand/__init__.py
+++ fand/__init__.py
@@ -8,26 +8,8 @@ __url__ = 'https://github.com/teapot9/fand'
 __version__ = '0.1.0'
 
 import logging
-import sys
-from typing import no_type_check
 
 logging.basicConfig(
     format='%(levelname)s:%(name)s: %(message)s',
     level=logging.NOTSET,
 )
-
-# Workaround pySMART bug with Python 3.8 logging API
-# Keep until pySMART's next release
-if sys.version_info >= (3, 8):
-    try:
-        import pySMART
-
-        class _WrapperPysmartLogger(pySMART.utils.TraceLogger):
-            @no_type_check
-            def findCaller(self, stack_info=False, stacklevel=1):
-                return super().findCaller(stack_info)
-
-        if logging.getLoggerClass() == pySMART.utils.TraceLogger:
-            logging.setLoggerClass(_WrapperPysmartLogger)
-    except ImportError:
-        pass
